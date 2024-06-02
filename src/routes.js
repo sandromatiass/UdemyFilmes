6c564ebf9c8b690a19bfeb6a9bb771493e077000
sandromatiass <devsandromatias@gmail.com>
2024-06-01 23:16:08 -0300

    project configuration

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./shared/pages/Home";
 import Filmes from "./shared/pages/Filmes";
 import Header from "./shared/components/Header";
+import Erro from "./shared/pages/Erro";
 
 function RoutesApp() {
   return (
@@ -11,6 +12,8 @@ function RoutesApp() {
       <Routes>
         <Route path="/" element={ <Home/> }/>
         <Route path="/filme/:id" element={ <Filmes/> } />
+
+        <Route path="*" element={ <Erro/> } />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/shared/pages/Erro/index.js
@@ -0,0 +1,9 @@
+function Erro() {
+  return(
+    <div>
+      <h1>Página não encontrada</h1>
+    </div>
+  )
+};
+
+export default Erro;
